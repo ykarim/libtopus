b91580bcc01577957392a836a427d5af64b524a1
Yusuf Karim <ykarim250@gmail.com>
2018-08-12 19:51:32 -0400

    Initialize Spring

---
+++ libtopus-core/src/main/java/org/ykarim/libtopuscore/LibtopusCoreApplication.java
@@ -0,0 +1,12 @@
+package org.ykarim.libtopuscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibtopusCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibtopusCoreApplication.class, args);
+    }
+}
---
+++ libtopus-core/src/test/java/org/ykarim/libtopuscore/LibtopusCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ykarim.libtopuscore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibtopusCoreApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
